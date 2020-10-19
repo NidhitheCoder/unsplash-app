e119bf1882d21fd2f15c0b14f9fc1fcac0d9bc45
NidhitheCoder <nidhpv@gmail.com>
2020-10-19 18:01:09 +0530

    navigation component created

--- unsplash-app/src/components/navbar/navbar.component.jsx
+++ unsplash-app/src/components/navbar/navbar.component.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import "./navbar.styles.modules.scss";
+import InputBase from "@material-ui/core/InputBase";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import SearchIcon from "@material-ui/icons/Search";
 
-const Navbar = () => <div>Navbar here</div>;
+const Navbar = () => (
+  <div className="header">
+    <div className="header-part">
+      <Typography className="logo">My Unsplash</Typography>
+      <div className="search">
+        <div className="search-icon">
+          <SearchIcon />
+        </div>
+        <InputBase placeholder="Search Name" className="search-bar" />
+      </div>
+    </div>
+    <div className="header-part">
+      <Button variant="contained" className="add-photo-btn">
+        Add Photo
+      </Button>
+    </div>
+  </div>
+);
 
 export default Navbar;
