8516182fe7065bb129b647e6ddd964450dc304f5
NidhitheCoder <nidhpv@gmail.com>
2020-10-20 17:53:21 +0530

    Login component created

---
+++ unsplash-app/src/pages/login-and-signUp/login-and-SignUp.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import  './login-and-signUp.styles.modules.scss';
+
+import Login from '../../components/login-component/login.component';
+
+const LoginAndSignUp = () => (
+    <Login />
+);
+
+export default LoginAndSignUp;
