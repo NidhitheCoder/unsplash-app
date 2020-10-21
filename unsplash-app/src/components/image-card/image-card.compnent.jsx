5ed5705c9b952e03b86abfa18dc4e6aa5065ec55
NidhitheCoder <nidhpv@gmail.com>
2020-10-21 16:40:58 +0530

    Image styles added

--- unsplash-app/src/components/image-card/image-card.compnent.jsx
+++ unsplash-app/src/components/image-card/image-card.compnent.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import "./image-card.styles.modules.scss";
 import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
+import {makeStyles} from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  card : {
+    borderRadius:theme.spacing(3)
+  }
+}))
 
 const ImageCard = ({ data }) => {
+  const classes = useStyles();
   return (
-    <Card className="image-card">
+    <Card className={`image-card ${classes.card}`}>
+    <div className="background-shade" />
       <CardMedia
         component="img"
         alt={data.title}
         image={data.imgUrl}
         title={data.title}
-      ></CardMedia>
+      />
       <h3> {data.title}</h3>
     </Card>
   );
