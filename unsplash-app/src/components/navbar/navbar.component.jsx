6b13f47e92d392c3cdb8ac5dcccb839a81680fc6
NidhitheCoder <nidhpv@gmail.com>
2020-10-22 13:04:41 +0530

    Modal styled

--- unsplash-app/src/components/navbar/navbar.component.jsx
+++ unsplash-app/src/components/navbar/navbar.component.jsx
@@ -4,28 +4,11 @@ import InputBase from "@material-ui/core/InputBase";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import SearchIcon from "@material-ui/icons/Search";
-import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import { makeStyles } from "@material-ui/core/styles";
-import { Fade } from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({
-  modal: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5]
-  }
-}));
-
-
+import ModalComponent from "../modal/modal.component";
 
 const Navbar = () => {
-  const classes = useStyles();
-  const [open,setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
   const handleOpen = () => {
     setOpen(true);
   };
@@ -46,30 +29,16 @@ const Navbar = () => {
         </div>
       </div>
       <div className="header-part">
-        <Button variant="contained" className="add-photo-btn" onClick={handleOpen}>
+        <Button
+          variant="contained"
+          className="add-photo-btn"
+          onClick={handleOpen}
+        >
           Add Photo
         </Button>
       </div>
-
-      <Modal
-        aria-labelledby="spring-modal-title"
-        aria-describedby="spring-modal-description"
-        open={open}
-        onClose={handleClose}
-        className={classes.modal}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500
-        }}
-      >
-        <Fade in={open}>
-          <div className={classes.paper}>
-            <h2 id="spring-modal-title">Spring Modal</h2>
-            <p id="spring-Modal-description">React spring animates me</p>
-          </div>
-        </Fade>
-      </Modal>
+      <ModalComponent open={open} handleClose={handleClose} >
+      </ModalComponent>
     </div>
   );
 };
